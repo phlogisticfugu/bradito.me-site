a9c53ef7068d921165b4629f5f0769a628d3591c
Brad Ito <phlogisticfugu@gmail.com>
2018-04-27 01:55:29 -0700

    fixing about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,15 +3,8 @@
     <v-container fluid fill-height>
       <v-layout justify-center align-center>
         <v-flex text-xs-center>
-          <v-tooltip left>
-            <v-btn slot="activator" :href="source" icon large 
-                   target="_blank">
-              <v-icon large>code</v-icon>
-            </v-btn>
-            <span>Source</span>
-          </v-tooltip>
           <v-tooltip right>
-            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/BYqXgr" 
+            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/BYqXgr"
                    target="_blank">
               <v-icon large>mdi-codepen</v-icon>
             </v-btn>
