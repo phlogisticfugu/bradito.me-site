9bc6a07514d6334e8c1a3d83fbaae743c007803c
Brad Ito <phlogisticfugu@gmail.com>
2023-08-11 10:13:29 -0700

    tweak footer to indicate template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => (
   <footer id="footer" className="wrapper style1-alt">
     <div className="inner">
       <ul className="menu">
-        <li>&copy; 2020. All rights reserved.</li>
+        <li>&copy; 2023. All rights reserved.</li>
         <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
+          Design template: <a href="http://html5up.net">HTML5 UP</a>
         </li>
       </ul>
     </div>
