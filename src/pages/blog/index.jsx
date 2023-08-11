a39aed70b892598acc08cb422316ff44cb818d05
Brad Ito <phlogisticfugu@gmail.com>
2023-08-11 10:13:45 -0700

    prep for more content on blog index

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -11,7 +11,9 @@ const IndexPage = ({
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-  return <div>{Posts}</div>
+  return (
+    <div>{Posts}</div>
+  )
 }
 
 export default IndexPage
