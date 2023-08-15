9a9667a0dda407e5f10a5adefc5e40ff4a872962
Brad Ito <phlogisticfugu@gmail.com>
2023-08-15 07:35:51 -0700

    adding current page active in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import '../assets/sass/main.scss';
-const Header = () => (
+
+const Header = () => {
+  const currentPathname = window?.location?.pathname || "";
+  return (
   <header id="header">
     <nav>
       <ul>
         <li>
-          <Link to="/">Main</Link>
+          <Link to="/" className={"/" === currentPathname ? "active": ""}>Main</Link>
         </li>
         <li>
-          <Link to="/blog/">Blog</Link>
+          <Link to="/blog/" className={currentPathname.startsWith("/blog/") ? "active": ""}>Blog</Link>
         </li>
       </ul>
     </nav>
   </header>
-);
+  );
+}
 
 export default Header;
