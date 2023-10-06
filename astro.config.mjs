4aa796bafb96649af66bdc2c1c4487cd9c4ddf2d
Brad Ito <phlogisticfugu@gmail.com>
2023-10-06 15:45:21 -0700

    adding @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 import favicons from "astro-favicons";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bradito.me',
-	integrations: [
-		mdx(),
-		sitemap(),
-		favicons({
-			masterPicture: "./src/website-icon.png",
-			emitAssets: true,
-			faviconsDarkMode: false,
-		})
-	],
+  site: 'https://bradito.me',
+  integrations: [mdx(), sitemap(), favicons({
+    masterPicture: "./src/website-icon.png",
+    emitAssets: true,
+    faviconsDarkMode: false
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
