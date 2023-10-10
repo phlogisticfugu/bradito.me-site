98507affa8879aee7d0b3bc3b85c883a8b72cc38
Brad Ito <phlogisticfugu@gmail.com>
2023-10-09 18:48:07 -0700

    set timezone explicitly for collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
+const timeZone = "America/Chicago";
+
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+		pubDate: z.coerce.date({ timeZone }),
+		updatedDate: z.coerce.date({ timeZone }).optional(),
 		heroImage: z.string().optional(),
 		status: z.enum(['draft', 'published']),
 	}),
