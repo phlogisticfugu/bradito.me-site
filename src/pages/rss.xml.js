44da7f2ad1608670c49b5dd3e27b3e6d065242ee
Brad Ito <phlogisticfugu@gmail.com>
2025-09-22 16:59:42 -0500

    applying auto-formatting

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,18 +3,16 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = (await getCollection('blog')).sort(
-		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-	).filter(
-		(post) => post.data.status === 'published'
-	);
-	return rss({
-		title: SITE_TITLE + " - Blog",
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = (await getCollection('blog'))
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .filter((post) => post.data.status === 'published');
+  return rss({
+    title: SITE_TITLE + ' - Blog',
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }
